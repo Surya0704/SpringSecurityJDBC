1bbf2b327940333bac2510870b6614769de7cda8
Surya0704 <47137297+Surya0704@users.noreply.github.com>
2022-08-22 00:01:10 +0530

    Initial commit

---
+++ PringSecurityJDBC/src/main/java/io/jvabrains/PringSecurityJDBC/PringSecurityJdbcApplication.java
@@ -0,0 +1,13 @@
+package io.jvabrains.PringSecurityJDBC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PringSecurityJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PringSecurityJdbcApplication.class, args);
+	}
+
+}
---
+++ PringSecurityJDBC/src/test/java/io/jvabrains/PringSecurityJDBC/PringSecurityJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jvabrains.PringSecurityJDBC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PringSecurityJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
